6c62e9586849d15f662f06c9e06431a5bb9319c0
Michael Bryce <michaelbryce@me.com>
2025-11-08 15:50:07 -0700

    fix: removed unused chains

--- packages/nextjs/utils/chains.ts
+++ packages/nextjs/utils/chains.ts
@@ -28,30 +28,30 @@ export const SUPPORTED_CHAINS: Record<string, ChainConfig> = {
     chain: sepolia,
     pollingInterval: 12_000, // 12 seconds
   },
-  holesky: {
-    id: "holesky",
-    name: "Holesky Testnet",
-    chain: holesky,
-    pollingInterval: 12_000, // 12 seconds
-  },
+  // holesky: {
+  //   id: "holesky",
+  //   name: "Holesky Testnet",
+  //   chain: holesky,
+  //   pollingInterval: 12_000, // 12 seconds
+  // },
   base: {
     id: "base",
     name: "Base",
     chain: base,
     pollingInterval: 2_000, // 2 seconds
   },
-  optimism: {
-    id: "optimism",
-    name: "Optimism",
-    chain: optimism,
-    pollingInterval: 2_000, // 2 seconds
-  },
-  arbitrum: {
-    id: "arbitrum",
-    name: "Arbitrum One",
-    chain: arbitrum,
-    pollingInterval: 100, // ~250ms block time
-  },
+  // optimism: {
+  //   id: "optimism",
+  //   name: "Optimism",
+  //   chain: optimism,
+  //   pollingInterval: 2_000, // 2 seconds
+  // },
+  // arbitrum: {
+  //   id: "arbitrum",
+  //   name: "Arbitrum One",
+  //   chain: arbitrum,
+  //   pollingInterval: 100, // ~250ms block time
+  // },
 };
 
 export const DEFAULT_CHAIN = "sepolia";
